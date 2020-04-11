ede2cab147ac5ec60c4297cb92cf6fa253590a09
AJayTheProgrammer <62700921+AJayTheProgrammer@users.noreply.github.com>
2020-04-11 11:51:54 -0400

    Stack Basics
    This program is to show the basics of a stack. Stacks follow the acronym of (LIFO).

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace StacksPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a stack 
+            // Using Stack class 
+            Stack myStack = new Stack();
+
+            // Adding elements in the Stack 
+            // Using Push method 
+            myStack.Push("Bottom");
+            myStack.Push("Middle");
+            myStack.Push("Top");
+            // 
+            // The Pop method is used to remove the most recent element added to the stack.
+            // Remove the comment slashes to see the effect of the Pop method.
+            // my_stack.Pop();
+         
+
+            // Accessing the elements 
+            // of my_stack Stack 
+            // Using foreach loop 
+            foreach (var elem in myStack)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+}
