a468e8f7a6982ec7bea3940c90381b97d817edb7
AJayTheProgrammer <62700921+AJayTheProgrammer@users.noreply.github.com>
2020-04-11 12:21:35 -0400

    Queue Basics
    This program demonstrates the basics of queues and how they can be manipulated.

---
+++ Queue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace QueueBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a queue 
+            // Using Queue class 
+            Queue myQueue = new Queue();
+
+            // Adding elements in Queue 
+            // Using Enqueue() method 
+            myQueue.Enqueue("Bob");
+            myQueue.Enqueue("Lacy");
+            myQueue.Enqueue("Greg");
+            myQueue.Enqueue("Cece");
+
+            // Accessing the elements 
+            // of myQueue Queue 
+            // Using foreach loop 
+            foreach (var ele in myQueue)
+            {
+                Console.WriteLine(ele);
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Total elements present in my_queue: {0}", myQueue.Count);
+
+            // Removes the first element in the queue. (LIFO)
+            myQueue.Dequeue();
+
+            // After Dequeue method 
+            Console.WriteLine("Total elements present in my_queue: {0}", myQueue.Count);
+
+            // Remove all the elements from the queue 
+            myQueue.Clear();
+
+            // After Clear method 
+            Console.WriteLine("Total elements present in my_queue: {0}",
+                                                        myQueue.Count);
+
+
+           
+        }
+    }
+}
